a04f027e13bb95226d3e68e68d9f86e4c0206cbc
Propulsive60r <70334601+Propulsive60r@users.noreply.github.com>
2021-02-09 22:46:48 +0300

    add components
    Services

---
+++ pages/components/services.js
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function Services() {
+    return <>
+        <h1>Hello RPK OKEY from NEXT JS</h1>
+        <p>
+            <Link href="../outdooradvertising"><a>НАРУЖНАЯ РЕКЛАМА</a></Link>
+        </p>
+        <p>
+            <Link href="../largeformatprinting"><a>ШИРОКОФОРМАТНАЯ ПЕЧАТЬ</a></Link>
+        </p>
+        <p>
+            <Link href="../typography"><a>ПОЛИГРАФИЯ</a></Link>
+        </p>
+    </>
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,8 @@
-import Link from "next/link";
+import Link from "next/link"
+import Services from "./components/services"
 
 export default function Index() {
     return <>
-        <h1>Hello RPK OKEY from NEXT JS</h1>
-        <p>
-            <Link href="outdooradvertising"><a>НАРУЖНАЯ РЕКЛАМА</a></Link>
-        </p>
-        <p>
-            <Link href="largeformatprinting"><a>ШИРОКОФОРМАТНАЯ ПЕЧАТЬ</a></Link>
-        </p>
-        <p>
-            <Link href="typography"><a>ПОЛИГРАФИЯ</a></Link>
-        </p>
+        <Services/>
     </>
 }
