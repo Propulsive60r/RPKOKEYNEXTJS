3694b653c0764028b880737f266c823456c5246e
Propulsive60r <70334601+Propulsive60r@users.noreply.github.com>
2021-02-28 16:01:16 +0300

    General Refactoring for Services
    1) Update base array
    2) Add const ServicesItems for component Services for use Props
    3) Add method Map to optimize the number of calls component Services

--- components/services/Services.js
+++ components/services/Services.js
@@ -2,89 +2,93 @@ import Link from "next/link"
 import s from './Services.module.scss'
 import Fade from 'react-reveal/Fade';
 
+//Массив с объектами
 
 const sBase = [
     {
         title: 'Наружная реклама',
-        description: 'Вам нужно сделать вывеску? Вам нужно оформить торговую площадку или офис? Вы желаете, чтобы клиент вас увидел и обратился к вам? Вы пришли по адресу.'
+        description: 'Вам нужно сделать вывеску? Вам нужно оформить торговую площадку или офис? Вы желаете, чтобы клиент вас увидел и обратился к вам? Вы пришли по адресу.',
+        link: '/outdooradvertising',
+        src: 'images/work/advout.jpg',
+        alt: 'Наружная реклама'
     },
     {
         title: 'Широкоформатная печать',
-        description: 'Печать баннеров, постеров, этикеток, наклеек. Для улицы, помещений. Резка пленки по контуру и в размер. Ламинирование.'
+        description: 'Печать баннеров, постеров, этикеток, наклеек. Для улицы, помещений. Резка пленки по контуру и в размер. Ламинирование.',
+        link: '/largeformatprinting',
+        src: 'images/work/printout.jpg',
+        alt: 'Широкоформатная печать'
     },
     {
         title: 'Полиграфия',
-        description: 'Изготовление любой печатной продукции. Листовки, флаеры, каталоги, буклеты, визитки, афиши, брошюры, журналы, календари.'
+        description: 'Изготовление любой печатной продукции. Листовки, флаеры, каталоги, буклеты, визитки, афиши, брошюры, журналы, календари.',
+        link: '/typography',
+        src: 'images/work/printout.jpg',
+        alt: 'Полиграфия'
     },
     {
         title: 'Монтажные работы',
-        description: 'Профессиональная группа мастеров-монтажников смонтируют для Вас любого уровня сложности наружную рекламу.'
+        description: 'Профессиональная группа мастеров-монтажников смонтируют для Вас любого уровня сложности наружную рекламу.',
+        link: '/montajout',
+        src: 'images/work/montajout.jpg',
+        alt: 'Монтажные работы'
     },
     {
         title: 'Создание сайтов',
-        description: 'Создадим уникальные сайты от простого одностраничного до интернет магазина. С использованием современных технологий. Поддержка и продвижение.'
+        description: 'Создадим уникальные сайты от простого одностраничного до интернет магазина. С использованием современных технологий. Поддержка и продвижение.',
+        link: '/webmade',
+        src: 'images/work/desighnWeb.jpg',
+        alt: 'Создание сайтов'
     },
     {
         title: 'Интернет-маркетинг',
-        description: 'Увеличить охват целевой аудитории? Увеличить конверсию аудитории в лиды? Увеличить конверсию лидов в продажи? Увеличить количество повторных продаж?'
+        description: 'Увеличить охват целевой аудитории? Увеличить конверсию аудитории в лиды? Увеличить конверсию лидов в продажи? Увеличить количество повторных продаж?',
+        link: '/webmarketing',
+        src: 'images/work/webMark.jpg',
+        alt: 'Интернет-маркетинг'
     }
 ]
 
-//разбить код на подкомпоненты
+//Описание основной компоненты одного ServiceItem. Пропсами описываем внутреннее содержание.
+
+const ServicesItems = (props) => {
+    return (
+        <>
+            <div className={s.servicesItemS}>
+                <div className={s.servicesItem}>
+                    <Link href={props.link}><a><img className={s.serviceImg}
+                                                    src={props.src}
+                                                    alt={props.alt}/></a>
+                    </Link>
+                </div>
+                <Link href={props.link}><a className={s.linkNoneColor}><h2>{props.title}</h2></a>
+                </Link>
+                <Link href={props.link}><a className={s.linkNoneColor}>
+                    <h3>{props.description}</h3></a>
+                </Link>
+            </div>
+        </>
+    )
+}
+
 export default function Services() {
+
+    //Использование метода MAP для уменьшения списка компонентов в JSX
+    // 6 -> 1
+
+    let serviceElements = sBase
+        .map(service => <ServicesItems title={service.title} description={service.description} link={service.link}
+                                       src={service.src} alt={service.alt}/>);
+
 
     return (
         <>
             <Fade>
-            <div className={s.services}>
-                <div className={s.servicesItemS}>
-                    <div className={s.servicesItem}>
-                        <Link href={'/outdooradvertising'}><a><img className={s.serviceImg}
-                                                                   src={'images/work/advout.jpg'}
-                                                                   alt="Наружная реклама"/></a>
-                        </Link>
-                    </div>
-                    <Link href={'/outdooradvertising'}><a className={s.linkNoneColor}><h2>{sBase[0].title}</h2></a>
-                    </Link>
-                    <Link href={'/outdooradvertising'}><a className={s.linkNoneColor}><h3>{sBase[0].description}</h3></a>
-                    </Link>
+                <div className={s.services}>
+
+                    {serviceElements}
+
                 </div>
-                <div className={s.servicesItemS}>
-                    <div className={s.servicesItem}>
-                        <img className={s.serviceImg} src={'images/work/printout.jpg'} alt="Широкоформатная печать"/>
-                    </div>
-                    <h2>{sBase[1].title}</h2>
-                    <h3>{sBase[1].description}</h3>
-                </div>
-                <div className={s.servicesItemS}>
-                    <div className={s.servicesItem}>
-                        <img className={s.serviceImg} src={'images/work/poliout.jpg'} alt="Полиграфия"/>
-                    </div>
-                    <h2>{sBase[2].title}</h2>
-                    <h3>{sBase[2].description}</h3>
-                </div>
-                <div className={s.servicesItemS}>
-                    <div className={s.servicesItem}>
-                        <img className={s.serviceImg} src={'images/work/montajout.jpg'} alt="Монтажные работы"/>
-                    </div>
-                    <h2>{sBase[3].title}</h2>
-                    <h3>{sBase[3].description}</h3>
-                </div>
-                <div className={s.servicesItemS}>
-                    <div className={s.servicesItem}>
-                        <img className={s.serviceImg} src={'images/work/desighnWeb.jpg'} alt="Создание сайтов"/>
-                    </div>
-                    <h2>{sBase[4].title}</h2>
-                    <h3>{sBase[4].description}</h3>
-                </div>
-                <div className={s.servicesItemS}>
-                    <div className={s.servicesItem}>
-                        <img className={s.serviceImg} src={'images/work/webMark.jpg'} alt="Интернет-маркетинг"/>
-                    </div>
-                    <h2>{sBase[5].title}</h2>
-                    <h3>{sBase[5].description}</h3>
-                </div>
-            </div>
             </Fade>
         </>
     )
